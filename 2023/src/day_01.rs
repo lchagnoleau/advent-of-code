3dd95d9fe3f91d12785d732e727a3a12c6baca10
Loïc Chagnoleau <furlings@pm.me>
2024-01-08 14:12:48 +0100

    add day 1 part 2

--- 2023/src/day_01.rs
+++ 2023/src/day_01.rs
@@ -1,9 +1,6 @@
 #[aoc(day1, part1)]
 fn part1(input: &str) -> u32 {
-    input
-        .lines()
-        .map(sum_first_and_last_digit)
-        .sum()
+    input.lines().map(sum_first_and_last_digit).sum()
 }
 
 fn sum_first_and_last_digit(line: &str) -> u32 {
@@ -14,13 +11,46 @@ fn sum_first_and_last_digit(line: &str) -> u32 {
     first_digit * 10 + last_digit
 }
 
+const TRANSLATIONS: [(&str, &str); 9] = [
+    ("one", "o1e"),
+    ("two", "t2o"),
+    ("three", "t3e"),
+    ("four", "f4r"),
+    ("five", "f5e"),
+    ("six", "s6x"),
+    ("seven", "s7n"),
+    ("eight", "e8t"),
+    ("nine", "n9e"),
+];
+
+#[aoc(day1, part2)]
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            TRANSLATIONS
+                .iter()
+                .fold(line.to_string(), |acc, (word, translation)| {
+                    acc.replace(word, translation)
+                })
+        })
+        .map(|line| sum_first_and_last_digit(line.as_str()))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn part1_example() {
+    fn part1_input() {
         let data = include_str!("../input/2023/day1.txt");
         assert_eq!(part1(data), 55108);
+    }
+
+    #[test]
+    fn part2_input() {
+        let data = include_str!("../input/2023/day1.txt");
+        assert_eq!(part2(data), 56324);
     }
 }
