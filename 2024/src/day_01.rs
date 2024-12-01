f89b1becb398a685ec28b86cb85ef9e3de1fd91d
Loïc Chagnoleau <furlings@pm.me>
2024-12-01 20:10:02 +0100

    2024 day1 part2

--- 2024/src/day_01.rs
+++ 2024/src/day_01.rs
@@ -29,6 +29,31 @@ fn split_line(line: &str) -> (u32, u32) {
     (first, second)
 }
 
+#[aoc(day1, part2)]
+fn part2(input: &str) -> u32 {
+    let mut result: u32 = 0;
+    let mut first_list = Vec::new();
+    let mut second_list = Vec::new();
+
+    for line in input.lines() {
+        let (first, second) = split_line(line);
+        first_list.push(first);
+        second_list.push(second);
+    }
+
+    for i in first_list.iter() {
+        let mut occurences = 0;
+        for j in second_list.iter() {
+            if i == j {
+                occurences += 1;
+            }
+        }
+        result += occurences * i;
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,6 +62,11 @@ mod tests {
     fn part1_input() {
         let data = include_str!("../input/day1.txt");
         assert_eq!(part1(data), 2285373);
+    }
+    #[test]
+    fn part2_input() {
+        let data = include_str!("../input/day1.txt");
+        assert_eq!(part2(data), 21142653);
     }
 
     #[test]
