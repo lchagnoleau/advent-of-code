f877efd6e41e884e5847d11b1a6eb9db4aec0d72
Loïc Chagnoleau <furlings@pm.me>
2024-12-03 11:44:54 +0100

    2024 day3 part2

--- 2024/src/day_03.rs
+++ 2024/src/day_03.rs
@@ -13,6 +13,23 @@ fn part1(input: &str) -> u32 {
     result
 }
 
+#[aoc(day3, part2)]
+fn part2(input: &str) -> u32 {
+    let mut result: u32 = 0;
+
+    let mut one_line = String::new();
+
+    for line in input.lines() {
+        one_line += line;
+    }
+
+    for mul in get_mul_tuples(delete_all_disable_inst(one_line.as_str()).as_str()).iter() {
+        result += mul.0 * mul.1;
+    }
+
+    result
+}
+
 fn get_mul_tuples(input: &str) -> Vec<(u32, u32)> {
     let mut result = Vec::new();
     let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
@@ -21,6 +38,11 @@ fn get_mul_tuples(input: &str) -> Vec<(u32, u32)> {
     }
 
     result
+}
+
+fn delete_all_disable_inst(input: &str) -> String {
+    let re = Regex::new(r"don't.*?(do[^n]|$)").unwrap();
+    re.replace_all(input, "").to_string()
 }
 
 #[cfg(test)]
@@ -34,9 +56,21 @@ mod tests {
     }
 
     #[test]
+    fn part2_input() {
+        let data = include_str!("../input/day3.txt");
+        assert_eq!(part2(data), 99532691);
+    }
+
+    #[test]
     fn test_get_mul_tuples() {
         let data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         let result = vec![(2, 4), (5, 5), (11, 8), (8, 5)];
         assert_eq!(get_mul_tuples(data), result);
+    }
+
+    #[test]
+    fn test_delete_all_disable_inst() {
+        let data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))do?mul(8,5))^don't()_mul(5,5)don't()_mul(5,5)";
+        assert_eq!(delete_all_disable_inst(data).as_str(), "xmul(2,4)&mul[3,7]!^)?mul(8,5))do?mul(8,5))^");
     }
 }
