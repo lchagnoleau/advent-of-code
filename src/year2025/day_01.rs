97268145bcfef86b694c309a4e77222906799940
Loïc Chagnoleau <furlings@pm.me>
2025-12-30 16:32:13 +0100

    2025 day1 part2

--- src/year2025/day_01.rs
+++ src/year2025/day_01.rs
@@ -1,5 +1,4 @@
 #[aoc(day1, part1)]
-
 fn part1(input: &str) -> u32 {
     let mut password: u32 = 0;
     let mut pointing: i32 = 50;
@@ -16,8 +15,30 @@ fn part1(input: &str) -> u32 {
     password
 }
 
+#[aoc(day1, part2)]
+fn part2(input: &str) -> u32 {
+    let mut password: u32 = 0;
+    let mut pointing: u32 = 1000000 + 50;
+
+    for line in input.lines() {
+        let clicks: u32 = line[1..].parse().unwrap();
+        let direction = line.as_bytes()[0];
+
+        for _ in 0..clicks {
+            if direction == b'R' {
+                pointing += 1;
+            } else {
+                pointing -= 1;
+            }
+            password += (pointing % 100 == 0) as u32;
+        }
+    }
+
+    password
+}
+
 #[test]
-fn test_part1() {
+fn test_day() {
     let data = "L68
 L30
 R48
@@ -29,4 +50,5 @@ L99
 R14
 L82";
     assert_eq!(part1(data), 3);
+    assert_eq!(part2(data), 6);
 }
